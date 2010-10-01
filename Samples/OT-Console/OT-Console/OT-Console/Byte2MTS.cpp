7af7495bdc35554877cf20609a7d508a9c05bef8
Joe Fitzpatrick <joe@innovate-tech.com>
2010-10-01 12:52:17 -0700

    Added basic OT Console app (built with VC++ 2005)

---
+++ Samples/OT-Console/OT-Console/OT-Console/Byte2MTS.cpp
@@ -0,0 +1,334 @@
+/*
+	Byte2MTS.cpp
+ 
+	Convert Byte Stream to MTS packets/information
+ 
+	This is a long, and unfortunately a bit complicated, state machine
+	that takes bytes in and syncs to an MTS data stream and parses
+	out data channels and query responses.
+ 
+	It is really an old 'C' module, but it is stuffed in a C++ class
+	so that it is easy to create more than one instance of it.
+ 
+	This module could be a lot simpler if LM-1 support was not included.
+*/
+
+// Includes ..................................................................
+
+#include "stdafx.h"
+#include "Byte2MTS.h"
+
+
+// Defines ...................................................................
+
+// Masks to help identify AFR packet types
+#define IDENTLM1(x) ((x & 0xA2) == 0x80)
+#define IDENTLC1(x) ((x & 0xE2) == 0x42)
+
+// The various 'states' our engine can be in
+
+typedef enum {
+	RXSYNCWAIT = 0, //0
+	RXGETLEN,		//1
+	RXGETCMDRESPONSE,	//2
+	RXWAITNXTPKT,	//3
+	RXLM1LOW,		//4
+	RXLAMBDAHIGH,	//5
+	RXLAMBDALOW,	//6
+	RXUBATH,		//7
+	RXUBATL,		//8
+	RXCMDRESPH,		//9
+	RXCMDRESPL,		//10
+}RXSTATE;
+
+
+#define LISTN_CMD_BYTE  	0xCC // 'L' with high bit set
+#define UNLSTN_CMD_BYTE 	0xEC // 'l' with high bit set
+
+
+ 
+// Constructor / Destructor ..................................................
+
+/*
+	Byte2MTS
+ 
+	Constructor, just initializes the states to good values
+*/
+
+Byte2MTS::Byte2MTS()
+{
+	rxstate = RXSYNCWAIT;
+	rcvLM1 = 0;
+	hdrword = 0;
+	rx2synccnt = 0;
+	rxlm1cnt = 0;
+}
+
+/*
+	~Byte2MTS
+ 
+	Destructor, empty
+*/
+
+Byte2MTS::~Byte2MTS()
+{
+}
+
+
+// Helper Functions ..........................................................
+
+/*
+	initRxPacket
+ 
+	Initialize our higher level packet for reception
+*/
+
+void Byte2MTS::initRxPacket(void)
+{
+	int i;
+	for (i = 0; i< MTS_MAX_CHANS; i++)
+		rxpacket.chan[i].func = MTS_FUNC_INVALID;
+	rxpacket.ubat = -1;
+	rxpacket.afrmult = -1;
+	rxpacket.numchans = 0;
+}
+
+
+// Public Interface ..........................................................
+
+/*
+	addByte
+ 
+	Feed a byte in, hopefully get parsed MTS packets out
+
+	Returns: TRUE if we have a new packet, FALSE if we are still looking
+*/
+
+U8 Byte2MTS::addByte(U8 cr)
+{
+	U8 syncerr = FALSE;
+	short rword;
+	
+	syncerr = FALSE;
+	switch(rxstate)
+	{
+		case RXSYNCWAIT: //waiting for next packet
+			hdrword <<= 8;
+			hdrword += cr;
+			rxcmdresp = 0;
+			if ((cr & 0xA2) == 0xA2) //Serial Prot. 2 header found
+			{
+				initRxPacket();
+				rxstate = RXGETLEN;
+				rxlen = (cr & 1) << 7;
+				rxhighbyte = cr;
+				rxserial1 = FALSE;
+				rcvLM1 = 0;
+			}
+			else if (((cr & 0xA2) == 0x80) && !(hdrword & 0x8000))//LM-1 packet header found
+			{
+				initRxPacket();
+				rxlen = 15; //LM-1 packet is 16 bytes, but we received one already
+				rxstate = RXLM1LOW;
+				rxhighbyte = cr;
+				rxserial1 = TRUE;
+				rcvLM1 = 6;
+			}	
+			break;
+		case RXGETLEN:
+			syncerr = !(cr & 0x80);
+			if (!syncerr)
+			{
+				rxlen += cr & 0x7F; //get length in bytes
+				rxlen <<= 1;
+				rxcmdresp = 0;
+				if (rxhighbyte & 0x10) //it's not a command response
+					rxstate = RXWAITNXTPKT; //but we wait to the end of this packet
+				else
+				{
+					rxstate = RXCMDRESPH; //interpret response command
+				}
+			}
+			break;
+		case RXGETCMDRESPONSE:
+			rxlen--;
+			response[rsplen] = cr;
+			rsplen++;
+			if (!rxlen)
+			{
+				rxstate = RXSYNCWAIT; //wait for next packet
+				return TRUE;
+			}
+			break;
+			
+			
+		case RXWAITNXTPKT:
+			rxlen--;
+			if (rxlen & 1) //high byte
+			{
+				rxhighbyte = cr;
+				if (IDENTLM1(cr) || IDENTLC1(cr))
+				{
+					rxstate = RXLM1LOW;
+					
+				}
+				else if (syncerr = (cr & 0x80))
+					break;
+				
+			}
+			else //low byte
+			{
+				if (syncerr = (cr & 0x80))
+					break;
+				rword = (rxhighbyte & 0x3F);
+				rword <<=7;
+				rword += cr & 0x7F;
+				rxpacket.chan[rxpacket.numchans].value = rword;
+				rxpacket.chan[rxpacket.numchans].lm1data = (rcvLM1 != 0);
+				if (rcvLM1)
+					rcvLM1--;
+				rxpacket.chan[rxpacket.numchans].func = MTS_FUNC_NOTLAMBDA;
+				rxpacket.numchans++;
+			}
+			
+			
+			if (!rxlen)
+			{
+				// received the data packet, add our stuff
+				hdrword = 0;
+				rxstate = RXSYNCWAIT; //wait for next packet
+				switch (rxcmdresp) 
+				{
+					case 0: // no command
+						if (rxserial1)
+						{
+							if (rxlm1cnt >= 1)
+								return TRUE;
+							rxlm1cnt++;
+						}
+						else
+						{
+							if (rx2synccnt >= 1)
+								return TRUE;
+							rx2synccnt++;
+						}
+						return FALSE;
+					case MTS_TYPELIST_QUERY:
+					case MTS_NAMELIST_QUERY:
+						break;
+					case LISTN_CMD_BYTE:
+						break;
+					case UNLSTN_CMD_BYTE:
+					default:
+						break;
+				}
+				
+				return TRUE; //signal that we got a packet
+			}
+			break;
+		case RXLM1LOW:
+			rxlen--;
+			if (syncerr = (cr & 0x80))
+				break;
+			rxpacket.chan[rxpacket.numchans].func = (rxhighbyte >> 2 ) & 7;
+			if (rxpacket.afrmult == -1)
+			{
+				rxpacket.afrmult = cr & 0x7F;
+				if (rxhighbyte & 1)
+					rxpacket.afrmult |= 0x80;
+			}
+			rcvLM1 = (IDENTLM1(rxhighbyte) ? 6:0);
+			
+			rxstate = RXLAMBDAHIGH;
+			break;
+		case RXLAMBDAHIGH:
+			rxlen--;
+			if (syncerr = (cr & 0x80))
+				break;
+			rxhighbyte = cr;
+			rxstate = RXLAMBDALOW;
+			break;
+		case RXLAMBDALOW:
+			rxlen--;
+			if (syncerr = (cr & 0x80))
+				break;
+			rword = (rxhighbyte & 0x3F);
+			rword <<=7;
+			rword += cr & 0x7F;
+			rxpacket.chan[rxpacket.numchans].value = rword;
+			
+			if (rxserial1 || rcvLM1)
+			{
+				rxpacket.chan[rxpacket.numchans].lm1data = 1;
+				if (rcvLM1)
+					rcvLM1--;
+				rxstate = RXUBATH;
+			}
+			else
+			{
+				rxpacket.chan[rxpacket.numchans].lm1data = (rcvLM1);
+				if (rxlen)
+					rxstate = RXWAITNXTPKT;
+				else
+				{
+					rxstate = RXSYNCWAIT;
+					if (rx2synccnt >= 3)
+					{
+						rxpacket.numchans++;
+						return TRUE;
+					}
+					rx2synccnt++;
+				}
+			}
+			rxpacket.numchans++;
+			break;
+		case RXUBATH:
+			rxlen--;
+			if (syncerr = (cr & 0x80))
+				break;
+			rxhighbyte = cr;
+			rxstate = RXUBATL;
+			break;
+		case RXUBATL:
+			rxlen--;
+			if (syncerr = (cr & 0x80))
+				break;
+		{
+			rword = (rxhighbyte & 7);
+			rword <<= 7;
+			rword += cr & 0x7F;
+			
+			float f = (float)((rxhighbyte >> 3) & 0x7);
+			f *= rword;
+			f *= 5;
+			f /= 1023;
+			rxpacket.ubat = f;
+		}
+			rxstate = RXWAITNXTPKT;
+			break;
+			
+		case RXCMDRESPH: //get high byte of command in the response
+			rxlen--;
+			
+			if (cr & 1)
+				rxcmdresp = 0x80;
+			else
+				rxcmdresp = 0;
+			rxstate = RXCMDRESPL;
+			break;
+		case RXCMDRESPL: //get low byte of command response
+			rxlen--;
+			rxcmdresp |= cr & 0x7F;
+			rsplen = 0; 
+			rxstate = RXGETCMDRESPONSE;
+			break;
+	}
+	if (syncerr)
+	{
+		rxstate = RXSYNCWAIT; //error in syncronisation
+		rx2synccnt = 0;
+		rxlm1cnt = 0;
+	}
+	
+	return FALSE;
+}
--- Samples/OT-Console/OT-Console/OT-Console/OT-Console.cpp
+++ Samples/OT-Console/OT-Console/OT-Console/OT-Console.cpp
@@ -13,8 +13,18 @@
 // Includes ..................................................................
 
 #include "stdafx.h"
-#include "imsnet.h"	// Network services
-#include "imsusb.h"	// USB services
+#include <conio.h>
+#include "ims.h"		// Basic MTS/OT-2 stuff
+
+#include "imspid.h"		// PID stuff
+#include "normpid.h"
+#include "NameUnitStrings.h"
+#include "MinMax.h"
+
+#include "imsnet.h"		// Network services
+#include "imsusb.h"		// USB services
+
+#include "Byte2Mts.h"	// MTS parser class
 
 
 // Defines ...................................................................
@@ -41,7 +51,17 @@ static void Flush();
 static bool IsByte();
 static BYTE GetByte();
 
+static void PrintMenu();
+static void PrintPrompt();
+
 static void SetupMode();
+
+static void ReadConfiguration();
+static void SetTempConfiguration();
+static void KillTempConfiguration();
+static void DumpData();
+static void TestCANExpertMode();
+
 
 // Public Functions ..........................................................
 
@@ -80,223 +100,67 @@ int _tmain(int argc, _TCHAR* argv[])
 	// Try to open a device
 	if (OpenConnection() != _OTC_NONE)
 	{
-		// Enter Setup Mode
-		// Important! setup and expert modes time out, so be careful
-		// single stepping in a debugger.
-		//
-		// Also, the SetupMode function assumes OT-2 and will loop forever
-		// if you are connected to something else
-		SetupMode();
+		// At this point, we expect the device to be in-band, streaming MTS packets
+		// so we will throw away data while we are waiting for the user to make a selection
+		PrintMenu();
 
-		// Enter expert mode
-		SendBytes(2, "e\x01");
-		unsigned char b = GetByte();	// Can take a LONG time! See SDK doc for details
-
-		int n = 0;
-		if (b == 1)
+		bool run = true;
+		while (run)
 		{
-			for (n=0 ; n<2000 ; n++)
+			PrintPrompt();
+
+			// Discard MTS stream and look for keystroke
+			while (1)
 			{
-				SendByte(0xFF);
-				b = GetByte();
-				if (b != 0xFF)
+				if (IsByte())
+					GetByte();
+
+				if (_kbhit())
 					break;
 			}
-		}
-		if (n != 2000)
-			printf("Expert Mode Error!\n");
 
-		// Dump our current rate
-		SendByte('r');
-		b = GetByte();
-
-		// Set the rate to 500 Kbit
-		SendByte('R');
-		SendByte(2);
-		b = GetByte();
-
-		// Toggle the LED on, then off
-		SendByte('L');
-		SendByte(1);
-		b = GetByte();
-
-		SendByte('L');
-		SendByte(0);
-		b = GetByte();
-
-		// Set an 'everything' filter
-		SendByte('F');
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		SendByte(0);
-		b = GetByte();
-
-		// Listen for 250 mS or up to 8 packets
-		SendByte('I');
-		SendByte(250);
-		SendByte(8);
-
-		while (1)
-		{
-			b = GetByte();
-			if (b == 0xFF)
-				break;
-
-			if (b)
-				printf("ext:");
-			else
-				printf("std:");
-
-			DWORD d;
-			((unsigned char *)(&d))[0] = GetByte();
-			((unsigned char *)(&d))[1] = GetByte();
-			((unsigned char *)(&d))[2] = GetByte();
-			((unsigned char *)(&d))[3] = GetByte();
-
-			printf("%08X ", d);
-
-			b = GetByte();
-			for (int i=0 ; i< b ; i++)
+			// Process our keystroke
+			switch (_getch())
 			{
-				unsigned char c;
+				case '1':
+					printf("Read Config\n\n");
+					ReadConfiguration();
+					break;
 
-				c = GetByte();
-				printf("%02x ", c);
+				case '2':
+					printf("Set Temp Config\n\n");
+					SetTempConfiguration();
+					break;
+
+				case '3':
+					printf("Kill Temp Config\n\n");
+					KillTempConfiguration();
+					break;
+
+				case '4':
+					printf("Dump Data\n\n");
+					DumpData();
+					break;
+
+				case '5':
+					printf("CAN Expert\n\n");
+					TestCANExpertMode();
+					break;
+
+				case '?':
+					printf ("Menu\n");
+					PrintMenu();
+					break;
+
+				case 0x1B:
+					printf("Exit\n");
+					run = false;
+					break;
+
+				default:
+					printf("Huh?\n");
 			}
-			printf("\n");
 		}
-
-		// Clear the filter
-		SendByte('f');
-		b = GetByte();
-
-		// Set 8 Handshake filters
-		for (int z=0 ; z < 8 ; z++)
-		{
-			DWORD dw;
-
-			// What we hear
-			dw = 0x7E8+z;
-
-			SendByte('E');
-			SendByte((BYTE)(dw &0xFF));
-			SendByte((BYTE)((dw >> 8) & 0xFF));
-			SendByte((BYTE)((dw >> 16) & 0xFF));
-			SendByte((BYTE)((dw >> 24) & 0xFF));
-
-			// Where we reply
-			dw = 0x7E0+z;
-			SendByte((BYTE)(dw &0xFF));
-			SendByte((BYTE)((dw >> 8) & 0xFF));
-			SendByte((BYTE)((dw >> 16) & 0xFF));
-			SendByte((BYTE)((dw >> 24) & 0xFF));
-
-			b = GetByte();
-		}
-
-// O<timeout><replies><ext><id><len><data>
-		unsigned char Packet[] = { 'O',		// Ouptut
-								   100,		// 100 ms (bigger than normal P2)
-								   8,		// Up to 8 responses
-								   0,		// Standard mode
-								   0xDF, 7, 0, 0, // We'll send on 0x7DF (little endian)
-								   8,		// We send 8 bytes (dlc = 8), even though we don't use all of them
-								   2, 1, 0, 0, 0, 0, 0, 0 };	// Payload, this is header (2 bytes, one packet)
-																// 1 (mode 1)
-																// pid 0
-		// Send it
-		SendBytes(sizeof(Packet), Packet);
-
-		// Print the results
-		printf("\n");
-
-		while (1)
-		{
-			b = GetByte();
-			if (b == 0xFF)
-				break;
-
-			if (b)
-				printf("ext:");
-			else
-				printf("std:");
-
-			DWORD d;
-			((unsigned char *)(&d))[0] = GetByte();
-			((unsigned char *)(&d))[1] = GetByte();
-			((unsigned char *)(&d))[2] = GetByte();
-			((unsigned char *)(&d))[3] = GetByte();
-
-			printf("%08X ", d);
-
-			b = GetByte();
-			for (int i=0 ; i< b ; i++)
-			{
-				unsigned char c;
-
-				c = GetByte();
-				printf("%02x ", c);
-			}
-			printf("\n");
-		}
-
-// Now something that normally gets a FC response...
-// O<timeout><replies><ext><id><len><data>
-		unsigned char Packet2[] = { 'O',		// Ouptut
-								   100,		// 100 ms (bigger than normal P2)
-								   8,		// Up to 8 responses
-								   0,		// Standard mode
-								   0xDF, 7, 0, 0, // We'll send on 0x7DF (little endian)
-								   8,		// We send 8 bytes (dlc = 8), even though we don't use all of them
-								   2, 9, 2, 0, 0, 0, 0, 0 };	// Payload, this is header (2 bytes, one packet)
-																// 9 (mode 9)
-																// 2 (VIN)
-		// Send it
-		SendBytes(sizeof(Packet2), Packet2);
-
-		// Print the results
-		printf("\n");
-
-		while (1)
-		{
-			b = GetByte();
-			if (b == 0xFF)
-				break;
-
-			if (b)
-				printf("ext:");
-			else
-				printf("std:");
-
-			DWORD d;
-			((unsigned char *)(&d))[0] = GetByte();
-			((unsigned char *)(&d))[1] = GetByte();
-			((unsigned char *)(&d))[2] = GetByte();
-			((unsigned char *)(&d))[3] = GetByte();
-
-			printf("%08X ", d);
-
-			b = GetByte();
-			for (int i=0 ; i< b ; i++)
-			{
-				unsigned char c;
-
-				c = GetByte();
-				printf("%02x ", c);
-			}
-			printf("\n");
-		}
-
-		// Exit Expert Mode
-		SendByte('s');
-
-		// Exit Setup Mode
-		SendByte('s');
 
 		// Close our connection
 		CloseConnection();
@@ -384,12 +248,29 @@ static int GetBytes(int len, void *buf)
 		return imsusbGetBytes(len, (BYTE *)buf);
 }
 
+static void PrintMenu()
+{
+	printf("\nMENU\n");
+	printf("<1> Read Configuration\n");
+	printf("<2> Set Temp Configuration\n");
+	printf("<3> Kill Temp Configuration\n");
+	printf("<4> Dump Data\n");
+	printf("<5> Test CAN expert mode\n");
+	printf("<?> Print this menu\n");
+	printf("<ESC> Exit\n");
+}
+
+static void PrintPrompt()
+{
+	printf("\nSelection> ");
+}
+
 // Enter setup mode
 // !!!! This will hang if we connected to something other than OT-2
 void SetupMode()
 {
 	// Go into setup mode
-	SendByte('S');
+	SendByte(MTS_SETUPMODE_CMD);
 	
 	// We could be behind, so look for "OT2 "
 	while (1)
@@ -414,4 +295,433 @@ void SetupMode()
 	// Eat the rest of the header
 	for (int i=0 ; i < 9 ; i++)
 		GetByte();
+}
+
+// Read and dump the configuration
+static void ReadConfiguration()
+{
+	OT_CONFIGURATION otconfig;
+
+	// Enter Setup Mode
+	// Important! setup and expert modes time out, so be careful
+	// single stepping in a debugger.
+	//
+	// Also, the SetupMode function assumes OT-2 and will loop forever
+	// if you are connected to something else
+	SetupMode();
+
+	// Get our current configuration
+	SendByte(OT_SETUP_GETCONFIG);
+	GetBytes(sizeof(otconfig), &otconfig);
+
+	// Exit Setup Mode
+	SendByte(OT_SETUP_EXIT);
+
+	printf("Protocol: ");
+	switch (otconfig.Protocol)
+	{
+		case OT_PROTO_AUTO:
+			printf("Automatic\n");
+			break;
+		case OT_PROTO_CAN:
+			printf("CAN\n");
+			break;
+		case OT_PROTO_PWM:
+			printf("PWM\n");
+			break;
+		case OT_PROTO_VPW:
+			printf("VPW\n");
+			break;
+		case OT_PROTO_ISO:
+			printf("ISO\n");
+			break;
+		default:
+			printf("?\n");
+			break;
+	}
+
+	printf("Channels: %d\n", otconfig.Channels);
+	for (int n=0 ; n<otconfig.Channels ; n++)
+	{
+		// Channel number
+		printf("%d: ", n+1);
+
+		// Name
+		printf(ObdiiName[otconfig.NormPid[n]]);
+
+		// Priority
+		if (otconfig.Flags & (1 << n))
+			printf(" (Low Priority)");
+
+		printf("\n");
+	}
+}
+
+// Set a tempoary coniguration (good for the life of this connection)
+// We do this so that the user's settings, which might be hooked to 
+// gauges or other products, isn't over written
+static void SetTempConfiguration()
+{
+	OT_CONFIGURATION otconfig;
+
+	printf("Setting temporary configuration\n");
+
+	// Enter Setup Mode
+	// Important! setup and expert modes time out, so be careful
+	// single stepping in a debugger.
+	//
+	// Also, the SetupMode function assumes OT-2 and will loop forever
+	// if you are connected to something else
+	SetupMode();
+
+	// Get our current configuration
+	SendByte(OT_SETUP_GETCONFIG);
+	GetBytes(sizeof(otconfig), &otconfig);
+
+	printf("Setting channels to 2\n");
+	otconfig.Channels = 2;
+
+	// Cars should have MAP or MAF
+	// We'll test for MAP and default to MAF
+	// This will also fail if no ECU connected
+	// You can also do this with the ECU PID bitmask, but that is
+	// more complicated
+	unsigned short normpid = NORM_PID_MAP;
+	SendByte(OT_SETUP_TESTNORMPID);
+	SendBytes(2, &normpid);
+	unsigned char b = GetByte();
+
+	if (b)
+	{
+		printf("Setting first channel to MAP\n");
+		otconfig.NormPid[0] = NORM_PID_MAP;
+	}
+	else
+	{
+		printf("Setting first channel to MAF\n");
+		otconfig.NormPid[0] = NORM_PID_MAF;
+	}
+
+	// RPM should always be available, so we won't test
+	printf("Setting second channel to RPM\n");
+	otconfig.NormPid[1] = NORM_PID_RPM;
+
+	SendByte(OT_SETUP_SETMYCONFIG);
+	SendBytes(sizeof(otconfig), &otconfig);
+	b = GetByte();	// <CR> response
+
+	// Exit Setup Mode
+	SendByte(OT_SETUP_EXIT);
+}
+
+static void KillTempConfiguration()
+{
+	printf("Killing temp configuration\n");
+
+	// Enter Setup Mode
+	// Important! setup and expert modes time out, so be careful
+	// single stepping in a debugger.
+	//
+	// Also, the SetupMode function assumes OT-2 and will loop forever
+	// if you are connected to something else
+	SetupMode();
+
+	SendByte(OT_SETUP_KILLMYCONFIG);
+	GetByte();	// <CR> response
+
+	// Exit Setup Mode
+	SendByte(OT_SETUP_EXIT);
+}
+
+// Dump MTS data as parsed packets
+// This assumes MTS data is flowing and will hang if it is not
+static void DumpData()
+{
+	printf("Reading and dumping 20 packets of MTS data\n");
+
+	// Yet again, we are going to get our configuration
+	// We could do this once and keep it around, but hey, this
+	// is a crappy command line tool...
+	//
+	// Anyway, we want it here so that we know how
+	// many of these channels are from the OT-2, not other
+	// MTS devices chained before it, and what normalized pid
+	// each channel represents
+
+	OT_CONFIGURATION otconfig;
+
+	// Enter Setup Mode
+	// Important! setup and expert modes time out, so be careful
+	// single stepping in a debugger.
+	//
+	// Also, the SetupMode function assumes OT-2 and will loop forever
+	// if you are connected to something else
+	SetupMode();
+
+	// Get our current configuration
+	SendByte(OT_SETUP_GETCONFIG);
+	GetBytes(sizeof(otconfig), &otconfig);
+
+	// Exit Setup Mode
+	SendByte(OT_SETUP_EXIT);
+
+	// Now, let's create an instance of the BYTE to parser class
+	Byte2MTS *parser = new Byte2MTS();
+
+	// And loop while we have less than 20 packets received
+	int packetcount = 0;
+	do
+	{
+		// Read a byte and feed it in until the parser
+		// indicates a received packet. The parser will actuall
+		// eat several packets at first to make sure we are really
+		// synced to the data stream
+		unsigned char b = GetByte();
+		if (parser->addByte(b))
+		{
+			// Make sure it isn't a response to an in-band command
+			if (!parser->rxcmdresp)
+			{
+				// Increment data packet count
+				packetcount++;
+
+				// OK, we now have an array of parsed data channels in our parser
+				// Let's skip channels that aren't from the OT-2 (if any)
+				// OT-2 channels will be last in chain
+				int channeloffset = parser->rxpacket.numchans - otconfig.Channels;
+
+				// First we'll dump raw values, this will be 10 bit samples (0-1023)
+				printf("RAW: ");
+
+				for (int n=0 ; n < otconfig.Channels ; n++)
+					printf("%d ", parser->rxpacket.chan[channeloffset + n].value);
+				printf("\n");
+
+				// Now we'll scale to float values for the PID's range
+				printf("SCALED: ");
+
+				for (int n=0 ; n < otconfig.Channels ; n++)
+				{
+					// First we'll figure out the range betweein the channels MIN and MAX
+					double min = NormPids[otconfig.NormPid[n]].min;
+					double max = NormPids[otconfig.NormPid[n]].max;
+
+					double range = max - min;
+
+					// Now we want to figure out how much range equates to our 10 bit sample
+					double value = parser->rxpacket.chan[channeloffset + n].value;
+
+					// We are 0-1023, so we'll divide by 1023.0 and then multiply by the range
+					value = (value / 1023.0) * range;
+
+					// Last we'll offset to min value
+					value += min;
+
+					printf("%0.2f %s ", value, NormPids[otconfig.NormPid[n]].units);
+				}
+				printf("\n");
+			}
+		}
+	} while(packetcount < 20);
+
+	// Free our parser
+	delete parser;
+}
+
+// Helper to dump read CAN packets in expert mode to the screen
+static void DumpCANPackets()
+{
+	// We will get at least an 0xFF from the unit, but
+	// we also could get a list of packets
+	printf("\n");
+
+	while (1)
+	{
+		unsigned char b = GetByte();
+		if (b == 0xFF)
+			break;
+
+		// Packet type
+		if (b)
+			printf("ext:");
+		else
+			printf("std:");
+
+		// ID
+		DWORD d;
+		((unsigned char *)(&d))[0] = GetByte();
+		((unsigned char *)(&d))[1] = GetByte();
+		((unsigned char *)(&d))[2] = GetByte();
+		((unsigned char *)(&d))[3] = GetByte();
+
+		printf("%08X ", d);
+
+		// Get length and dump data
+		b = GetByte();
+		for (int i=0 ; i< b ; i++)
+		{
+			unsigned char c;
+
+			c = GetByte();
+			printf("%02x ", c);
+		}
+		printf("\n");
+	}
+}
+
+// Go into expert mode and try sniffing and some other
+// things on the CAN bus
+static void TestCANExpertMode()
+{
+	// Enter Setup Mode
+	// Important! setup and expert modes time out, so be careful
+	// single stepping in a debugger.
+	//
+	// Also, the SetupMode function assumes OT-2 and will loop forever
+	// if you are connected to something else
+	SetupMode();
+
+	// Enter expert mode
+	printf("Entering CAN Expert Mode\n");
+
+	SendBytes(2, "e\x01");
+	unsigned char b = GetByte();	// Can take a LONG time! See SDK doc for details
+
+	if (b != 1)
+	{
+		printf("Error entering CAN expert MODE!\n");
+		return;
+	}
+
+	// Dump our current rate
+	SendByte('r');
+	b = GetByte();
+	printf("Current bitrate is: %d\n", b);
+
+	// Set the rate to 500 Kbit
+	SendByte('R');
+	SendByte(2);
+	b = GetByte();
+	printf("Set bitrate to: %d\n", b);
+
+	// Toggle the LED on, then off
+	// No particular reason, just because we can...
+	printf("Vehicle LED on\n");
+	SendByte('L');
+	SendByte(1);
+	b = GetByte();	// Be sure to eat the response!
+
+	// We want to wait, but we don't want to trigger
+	// the timeout by not communicating, so we'll send the keep alive
+	// command (0xFF)
+	for (int n=0 ; n<1000 ; n++)
+	{
+		SendByte(0xFF);
+		b = GetByte();
+		if (b != 0xFF)
+		{
+			printf("CAN keepalive error!\n");
+			return;
+		}
+	}
+
+	// And off
+	printf("Vehicle LED off\n");
+	SendByte('L');
+	SendByte(0);
+	b = GetByte();	// Be sure to eat response!
+
+	// Now let's just 'sniff' the bus for activity
+	printf("Sniffing Bus for 250 ms or 8 packets\n");
+
+	// Set an 'everything' filter, get response
+	SendBytes(9, "F\x00\x00\x00\x00\x00\x00\x00\x00");
+	b = GetByte();
+
+	// Listen for 250 mS or up to 8 packets
+	SendByte('I');
+	SendByte(250);
+	SendByte(8);
+
+	// Dump Can Packets (if any)
+	DumpCANPackets();
+
+	// Clear the filter
+	SendByte('f');
+	b = GetByte();
+
+	// Try to read a PID
+	printf("Try reading an OBD-II J1979 PID\n");
+
+	// Set 8 Handshake filters
+	// for standard ECU responses
+	// Note, it is BETTER, network/speed wise, to use
+	// SendBytes for a constructed message, but
+	// I used SendByte here for clarity of what is being
+	// constructed.
+	for (int z=0 ; z < 8 ; z++)
+	{
+		DWORD dw;
+
+		// What we hear
+		dw = 0x7E8+z;
+
+		SendByte('E');
+		SendByte((BYTE)(dw &0xFF));
+		SendByte((BYTE)((dw >> 8) & 0xFF));
+		SendByte((BYTE)((dw >> 16) & 0xFF));
+		SendByte((BYTE)((dw >> 24) & 0xFF));
+
+		// Where we reply
+		dw = 0x7E0+z;
+		SendByte((BYTE)(dw &0xFF));
+		SendByte((BYTE)((dw >> 8) & 0xFF));
+		SendByte((BYTE)((dw >> 16) & 0xFF));
+		SendByte((BYTE)((dw >> 24) & 0xFF));
+
+		b = GetByte();
+	}
+
+	// Now we'll send output, and look for a response
+	// This time we construct a packet and send it in one swoop
+	// O<timeout><replies><ext><id><len><data>
+	unsigned char Packet[] = { 'O',		// Ouptut
+							   100,		// 100 ms (bigger than normal P2)
+							   8,		// Up to 8 responses
+							   0,		// Standard mode
+							   0xDF, 7, 0, 0, // We'll send on 0x7DF (little endian)
+							   8,		// We send 8 bytes (dlc = 8), even though we don't use all of them
+							   2, 1, 0, 0, 0, 0, 0, 0 };	// Payload, this is header (2 bytes, one packet)
+															// 1 (mode 1)
+															// pid 0
+	// Send it
+	SendBytes(sizeof(Packet), Packet);
+
+	// Print the results
+	DumpCANPackets();
+
+	// Now something that normally gets a Flow control response...
+	printf("Try to read VIN (not available on all vehicles)\n");
+
+	// O<timeout><replies><ext><id><len><data>
+	unsigned char Packet2[] = { 'O',		// Ouptut
+							   100,		// 100 ms (bigger than normal P2)
+							   8,		// Up to 8 responses
+							   0,		// Standard mode
+							   0xDF, 7, 0, 0, // We'll send on 0x7DF (little endian)
+							   8,		// We send 8 bytes (dlc = 8), even though we don't use all of them
+							   2, 9, 2, 0, 0, 0, 0, 0 };	// Payload, this is header (2 bytes, one packet)
+															// 9 (mode 9)
+															// 2 (VIN)
+	// Send it
+	SendBytes(sizeof(Packet2), Packet2);
+
+	// Print the results
+	DumpCANPackets();
+
+	// Exit Expert Mode
+	SendByte(OT_SETUP_EXIT);
+
+	// Exit Setup Mode
+	SendByte(OT_SETUP_EXIT);
 }
